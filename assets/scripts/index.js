105b03fdd25ab144d52262b9be5e322335492c3e
Jacqueline Yeung <jtyeung168@berkeley.edu>
2019-08-26 07:56:29 -0700

    fixed announcements, need to finish guides, worksheets, about me

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,40 +2,176 @@ $(document).ready(function() {
 	let one = document.getElementById("1");
 	let two = document.getElementById("2");
 	let three = document.getElementById("3");
+	let four = document.getElementById("4");
+	let five = document.getElementById("5");
+	let six = document.getElementById("6");
+	let seven = document.getElementById("7");
+	let eight = document.getElementById("8");
+	let nine = document.getElementById("9");
+	let ten = document.getElementById("10");
+	let eleven = document.getElementById("11");
+	let twelve = document.getElementById("12");
+	let thirteen = document.getElementById("13");
+	let fourteen = document.getElementById("14");
+	let fifteen = document.getElementById("15");
+	let sixteen = document.getElementById("16");
+	let seventeen = document.getElementById("17");
 
-	$(one).hide();
+	$(one).show();
 	$(two).hide();
-	$(three).show();
+	$(three).hide();
+	$(four).hide();
+	$(five).hide();
+	$(six).hide();
+	$(seven).hide();
+	$(eight).hide();
+	$(nine).hide();
+	$(ten).hide();
+	$(eleven).hide();
+	$(twelve).hide();
+	$(thirteen).hide();
+	$(fourteen).hide();
+	$(fifteen).hide();
+	$(sixteen).hide();
+	$(seventeen).hide();
 
 	// $(".eraser-img").click(function() {
 	$(".chalkb").click(function() {
 		let hasOne = $(this).hasClass("1");
 		let hasTwo = $(this).hasClass("2");
+		let hasThree = $(this).hasClass("3");
+		let hasFour = $(this).hasClass("4");
+		let hasFive = $(this).hasClass("5");
+		let hasSix = $(this).hasClass("6");
+		let hasSeven = $(this).hasClass("7");
+		let hasEight = $(this).hasClass("8");
+		let hasNine = $(this).hasClass("9");
+		let hasTen = $(this).hasClass("10");
+		let hasEleven = $(this).hasClass("11");
+		let hasTwelve = $(this).hasClass("12");
+		let hasThirteen = $(this).hasClass("13");
+		let hasFourteen = $(this).hasClass("14");
+		let hasFifteen = $(this).hasClass("15");
+		let hasSixteen = $(this).hasClass("16");
 
 		if(hasOne) {
 			$(one).show();
 			$(two).hide();
-			$(three).hide();
 		} else if (hasTwo) {
-			$(one).hide();
 			$(two).show();
 			$(three).hide();
-		}
+		} else if (hasThree) {
+			$(three).show();
+			$(four).hide();
+		} else if (hasFour) {
+			$(four).show();
+			$(five).hide();
+		} else if (hasFive) {
+			$(five).show();
+			$(six).hide();
+		} else if (hasSix) {
+			$(six).show();
+			$(seven).hide();
+		} else if (hasSeven) {
+			$(seven).show();
+			$(eight).hide();
+		} else if (hasEight) {
+			$(eight).show();
+			$(nine).hide();
+		} else if (hasNine) {
+			$(nine).show();
+			$(ten).hide();
+		} else if (hasTen) {
+			$(ten).show();
+			$(eleven).hide();
+		} else if (hasEleven) {
+			$(eleven).show();
+			$(twelve).hide();
+		} else if (hasTwelve) {
+			$(twelve).show();
+			$(thirteen).hide();
+		} else if (hasThirteen) {
+			$(thirteen).show();
+			$(fourteen).hide();
+		} else if (hasFourteen) {
+			$(fourteen).show();
+			$(fifteen).hide();
+		} else if (hasFifteen) {
+			$(fifteen).show();
+			$(sixteen).hide();
+		} else if (hasSixteen) {
+			$(sixteen).show();
+			$(seventeen).hide();
+		} 
 	})
 
 	$(".chalk").click(function() {
-		let hasThree = $(this).hasClass("3");
 		let hasTwo = $(this).hasClass("2");
+		let hasThree = $(this).hasClass("3");
+		let hasFour = $(this).hasClass("4");
+		let hasFive = $(this).hasClass("5");
+		let hasSix = $(this).hasClass("6");
+		let hasSeven = $(this).hasClass("7");
+		let hasEight = $(this).hasClass("8");
+		let hasNine = $(this).hasClass("9");
+		let hasTen = $(this).hasClass("10");
+		let hasEleven = $(this).hasClass("11");
+		let hasTwelve = $(this).hasClass("12");
+		let hasThirteen = $(this).hasClass("13");
+		let hasFourteen = $(this).hasClass("14");
+		let hasFifteen = $(this).hasClass("15");
+		let hasSixteen = $(this).hasClass("16");
+		let hasSeventeen = $(this).hasClass("17");
 
-		if(hasThree) {
-			$(one).hide();
-			$(two).hide();
-			$(three).show();
-		} else if (hasTwo) {
-			$(one).hide();
+		if (hasTwo) {
 			$(two).show();
+			$(one).hide();
+		} else if (hasThree) {
+			$(three).show();
+			$(two).hide();
+		} else if (hasFour) {
+			$(four).show();
 			$(three).hide();
-		}
+		} else if (hasFive) {
+			$(five).show();
+			$(four).hide();
+		} else if (hasSix) {
+			$(six).show();
+			$(five).hide();
+		} else if (hasSeven) {
+			$(seven).show();
+			$(six).hide();
+		} else if (hasEight) {
+			$(eight).show();
+			$(seven).hide();
+		} else if (hasNine) {
+			$(nine).show();
+			$(eight).hide();
+		} else if (hasTen) {
+			$(ten).show();
+			$(nine).hide();
+		} else if (hasEleven) {
+			$(eleven).show();
+			$(ten).hide();
+		} else if (hasTwelve) {
+			$(twelve).show();
+			$(eleven).hide();
+		} else if (hasThirteen) {
+			$(thirteen).show();
+			$(twelve).hide();
+		} else if (hasFourteen) {
+			$(fourteen).show();
+			$(thirteen).hide();
+		} else if (hasFifteen) {
+			$(fifteen).show();
+			$(fourteen).hide();
+		} else if (hasSixteen) {
+			$(sixteen).show();
+			$(fifteen).hide();
+		} else if (hasSeventeen) {
+			$(seventeen).show();
+			$(sixteen).hide();
+		} 
 	})
 
 });
--- assets/scripts/wksheet.js
+++ assets/scripts/wksheet.js
@@ -2,13 +2,26 @@ $(document).ready(function() {
 
 	var slider1 = document.getElementById("soln1");
 	var slider2 = document.getElementById("soln2");
-	var slider3 = document.getElementById("soln3")
+	var slider3 = document.getElementById("soln3");
+	// var slider4 = document.getElementById("soln1");
+	// var slider5 = document.getElementById("soln2");
+	// var slider6 = document.getElementById("soln3");
+	// var slider7 = document.getElementById("soln1");
+	// var slider8 = document.getElementById("soln2");
+	// var slider9 = document.getElementById("soln3");
+	// var slider10 = document.getElementById("soln1");
+	// var slider11 = document.getElementById("soln2");
+	// var slider12 = document.getElementById("soln3");
+	// var slider13 = document.getElementById("soln1");
+	// var slider14 = document.getElementById("soln2");
+	// var slider15 = document.getElementById("soln3");
+
 
 
 	function getSoln1() {
 		let val1 = slider1.value;
 		var solnstr1 = "View Solution";
-		var solnLink1 = solnstr1.link("https://drive.google.com/file/d/1iCNsA4oY4nAucU4iCI9UdcmD6_zHh0uk/view?usp=sharing");
+		var solnLink1 = solnstr1.link("");
 		if(val1 == 100) {
 			document.getElementById("soln1link").innerHTML = solnLink1;
 		} else {
@@ -19,7 +32,7 @@ $(document).ready(function() {
 	function getSoln2() {
 		let val2 = slider2.value;
 		var solnstr2 = "View Solution";
-		var solnLink2 = solnstr2.link("https://drive.google.com/file/d/1oUevDtiIc69Z0vH-VVoHB-fegxS6PZIb/view?usp=sharing");
+		var solnLink2 = solnstr2.link("");
 		if(val2 == 100) {
 			document.getElementById("soln2link").innerHTML = solnLink2;
 		} else {
@@ -30,7 +43,7 @@ $(document).ready(function() {
 	function getSoln3() {
 		let val3 = slider3.value;
 		var solnstr3 = "View Solution";
-		var solnLink3 = solnstr3.link("https://drive.google.com/file/d/1Eid5QBZO-XszA2A7p10Ayo0iHTxLBcD5/view?usp=sharing");
+		var solnLink3 = solnstr3.link("");
 		if(val3 == 100) {
      		document.getElementById("soln3link").innerHTML = solnLink3;
 		} else {
